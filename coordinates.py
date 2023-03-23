31cae286b94aaad5e0d2cf72426b291e37b851ac
Krishang Naikar <krishangnaikar7@gmail.com>
2023-03-23 16:16:15 -0700

    Delete coordinates.py

--- coordinates.py
+++
@@ -1,9 +0,0 @@
-# This program prints out the coordinates of the mouse cursor, this can be used to tweak the values
-import pyautogui
-
-# Get the current position of the mouse cursor
-x, y = pyautogui.position()
-
-# Print the coordinates
-print("x-coordinate:", x)
-print("y-coordinate:", y)
